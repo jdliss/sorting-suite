82483a3c01726662b7a2ddf4f9f3f3cde0f96b38
Jonathan <jonacom@lissismore.com>
2016-02-13 17:16:00 -0700

    finished merge sort

---
+++ merge_sort.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class MergeSort
+  def sort(array)
+    if array.length <= 1
+      return array
+
+    else
+      middle = (array.length / 2).floor
+      left = sort(array[0..middle-1])
+      right = sort(array[middle..array.length])
+      return merge(left, right)
+    end
+  end
+
+  def merge(left, right)
+    if left.empty?
+      return right
+    elsif right.empty?
+      return left
+    elsif left.first < right.first
+      return [left.first] + merge(left[1..left.length], right)
+    else
+      return [right.first] + merge(left, right[1..right.length])
+    end
+  end
+end
